19171f8a843f94e8509c43c17a202fd8724619f2
Shubham <shubhamkokode11697@gmail.com>
2023-02-16 20:55:57 +0530

    Changes made in m2 method

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 class Test{
     public void m2(){
-        System.out.println("Shubham");
+        System.out.println("Shubham Kokode");
     }
 public static void main(String args[]){
     System.out.println("Shubham Kokode");
-}
-}
+}}
